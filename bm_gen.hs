6bc373b617b4e844afb980abd2a494b955000199
EDHidalgo <ehidalgo@ku.edu>
2018-10-17 17:09:33 -0500

    Fixed the main function to be more functional..ly... written, significant performance improvements achieved

--- bm_gen.hs
+++ bm_gen.hs
@@ -1,45 +1,24 @@
---a = [[1, 2], [1, 2], [1, 2], [1, 2]]
-
 import System.IO
 import Data.WAVE
 import Data.List
-import Data.Int
 
-import System.Process
+import System.Process -- I took out parallel processing, put it back in
 import Control.Parallel
-import System.Exit
 
--- 7791616
--- ^ Current song length
--- Filter the results!
+-- TODO
+-- 1) Filter the results so that we have a minimum threshold
+-- 2) Get results from the right (?) channel as well, we only get them from the left channel ATM
 
 main = do
    h <- openFile "song.wav" ReadMode
    wav <- getWAVEFile "song.wav"
    let samples = waveSamples wav
-   let song_length = (length samples)
 
-   let halfLength = (song_length `div` 2)
-   let firstHalf  = take halfLength samples
-   let secondHalf = drop halfLength samples
-
-   let quartLength = halfLength `div` 2 -- Or is song_length / 4 better?
- 
-   --let q1 = take quartLength firstHalf
---   let q2 = drop quartLength firstHalf
---   let q3 = take quartLength secondHalf
---   let q4 = drop quartLength secondHalf
-
---   leftChannel only ATM
-
---   let imp1 = (listImpulses (firstHalf `div` 2)) `par` (listImpulses ( `div` 2))
-   let imp1 = (listImpulses (take quartLength firstHalf)) `par` (listImpulses (drop quartLength firstHalf))
-   let imp2 = (listImpulses (take quartLength secondHalf)) `par` (listImpulses (drop quartLength secondHalf))
-
-   print (length imp1)
+   let imp = listImpulses samples
+   return (length imp)
 
 
---listImpulses :: [WAVESample] -> [WAVESample]
+listImpulses :: [[WAVESample]] -> [WAVESample]
 
 listImpulses [] = []
 listImpulses xs = [((h' - t') `div` 5000)] ++ (listImpulses (drop 1470 xs))
