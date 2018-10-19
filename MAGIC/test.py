9572dc7063068ab54886cc9deeb0cda7bda62c26
EDHidalgo <ehidalgo@ku.edu>
2018-10-18 19:20:00 -0500

    MAGIC COMMIT

---
+++ MAGIC/test.py
@@ -0,0 +1,3 @@
+import myMod
+
+print(myMod.version())
--- bm_gen.hs
+++ bm_gen.hs
@@ -8,6 +8,8 @@ import Control.Parallel
 -- TODO
 -- 1) Filter the results so that we have a minimum threshold
 -- 2) Get results from the right (?) channel as well, we only get them from the left channel ATM
+-- 3) Output the results in a Python readable format
+-- 4) Be callable!?!?!?
 
 main = do
    h <- openFile "song.wav" ReadMode
@@ -22,5 +24,6 @@ listImpulses :: [[WAVESample]] -> [WAVESample]
 
 listImpulses [] = []
 listImpulses xs = [((h' - t') `div` 5000)] ++ (listImpulses (drop 1470 xs))
+-- The division by 5000 is there because these numbers are stupid big and annoying to look at
    where h' = foldl' (+) 0 [h | h <- map (\a -> head a) (take 735 xs)]
          t' = foldl' (+) 0 [t | t <- map (\b -> last b) (take 735 (drop 735 xs))]
