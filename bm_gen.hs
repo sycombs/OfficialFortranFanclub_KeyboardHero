844adb0f91a207119570298b4063144c48992b8e
EDHidalgo <ehidalgo@ku.edu>
2018-10-20 05:35:51 -0500

    Unsure about the 'activation frames' being listed properly, but right now I'm calibrating the number of impulses we get

--- bm_gen.hs
+++ bm_gen.hs
@@ -18,35 +18,38 @@ main = do
    wav <- getWAVEFile "song.wav"
    let samples = waveSamples wav
 
-   let leftChan = lChan samples
-   --let imps = (s' leftChan)
-   return leftChan
-   --let imp = --filter (>350000) (map abs (listImpulses samples))
-   --let imp = listImpulses samples
-   --return imp
+   let l  = lChan samples
+   let r  = rChan samples
 
+   let lImps = listImp (s' l)
+   let rImps = listImp (s' r)
 
+   return lImps
 
 lChan :: [[WAVESample]] -> [WAVESample]
 lChan cs = [l | l <- map (\x -> head x) cs]
 
---rChan :: [a] -> a
---rChan cs = [r | r <- map (\x -> last x) cs]
+rChan :: [[WAVESample]] -> [WAVESample]
+rChan cs = [r | r <- map (\x -> last x) cs]
 
 --s' :: [WAVESample] -> [Integer]
 s' [] = []
 s' xs = [d'] ++ s' (drop 1470 xs)
-  where d' = diff x - diff y
-        x = take 735 xs
-        y = take 735 (drop 735 xs)
+  where d' = (diff x - diff y) `div` 5000
+        p = take 1470 xs
+        x = take 735 p
+        y = drop 735 p
+        --x = take 735 xs
+        --y = take 735 (drop 735 xs)
 --s' a = [sum (take 2 a)] ++ (s' (drop 2 a))
 
 --diff :: [Integer] -> Integer
 diff [] = 0
-diff xs = foldl' (+) 0 xs-- ++ (diff (drop 2 xs))
-  --where d' = (take 1 xs) - (take 1 (drop 1 xs))
+diff xs = foldl' (+) 0 xs
 
-listImp xs = findIndices (> 350000) xs
+listImp xs = map (\x -> x * 1470) (findIndices (> 300000) (map abs xs))
+--findIndices (> 300000) (map abs xs)
+
 
 
 -- My list is all the differences along with where they happen
