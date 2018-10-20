e4a545b9a85b46e269c4a2cf516446eddd2809d9
EDHidalgo <ehidalgo@ku.edu>
2018-10-20 07:58:41 -0500

    Added some formatting to the hOES

--- bm_gen.hs
+++ bm_gen.hs
@@ -1,16 +1,23 @@
 import System.IO
 import Data.WAVE
 import Data.List
-import Data.String
-import System.Posix.Temp
 
 main = do
    h <- openFile "song.wav" ReadMode
    wav <- getWAVEFile "song.wav"
-   return (waveSamples wav)
+   -- hOES: Holding Output EntrieS
+   let hOES = pImps (waveSamples wav)
+   let a = putStr (h' hOES)
+   a
 
 -- Print Impulses to a file
---pImps
+pImps p = gImps (part (splitChan p))
+
+-- Get Impulses
+gImps xs = map (\x -> x * 1470) (findIndices (> 350000) (map abs xs))
+
+h' [] = []
+h' (e:es) = ("Activation Frame: " ++ (show e) ++ "\n") ++ (h' es)
 
 splitChan cs = [samp | samp <- map (\x -> head x) cs]
 
@@ -19,6 +26,3 @@ part xs = [d'] ++ part (drop 1470 xs)
   where d' = (x - y) `div` 5000
         x = foldl' (+) 0 (take 735 xs)
         y = foldl' (+) 0 (take 735 (drop 735 xs))
-
--- Get Impulses
-gImps xs = map (\x -> x * 1470) (findIndices (> 350000) (map abs xs))
