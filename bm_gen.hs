51c70209aac30af56f0ec0e36bfea6513779880e
EDHidalgo <ehidalgo@ku.edu>
2018-10-20 06:59:32 -0500

    Added more descriptive names for two functions. pImps : Print impulses, and gImps : Get impulses

--- bm_gen.hs
+++ bm_gen.hs
@@ -1,18 +1,24 @@
 import System.IO
 import Data.WAVE
 import Data.List
+import Data.String
+import System.Posix.Temp
 
 main = do
    h <- openFile "song.wav" ReadMode
    wav <- getWAVEFile "song.wav"
    return (waveSamples wav)
 
+-- Print Impulses to a file
+--pImps
+
 splitChan cs = [samp | samp <- map (\x -> head x) cs]
 
 part [] = []
 part xs = [d'] ++ part (drop 1470 xs)
-  where d' = ( (foldl' (+) 0 x) - (foldl' (+) 0 y) ) `div` 5000
-        x = take 735 xs
-        y = take 735 (drop 735 xs)
+  where d' = (x - y) `div` 5000
+        x = foldl' (+) 0 (take 735 xs)
+        y = foldl' (+) 0 (take 735 (drop 735 xs))
 
-lImps xs = map (\x -> x * 1470) (findIndices (> 350000) (map abs xs))
+-- Get Impulses
+gImps xs = map (\x -> x * 1470) (findIndices (> 350000) (map abs xs))
