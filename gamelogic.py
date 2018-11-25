6a79d3327309da8f3513d1dd69e02690a94b65ec
Sydney <sydneyycombs@gmail.com>
2018-11-25 16:17:02 -0600

    Updated standard mode

--- gamelogic.py
+++ gamelogic.py
@@ -13,6 +13,7 @@ import hollow
 
 class gamelogic:
     note_list = []
+    map = []            #standard mode only
     def generate(self, difficulty=1):
         f = open("cry.json", 'r')
         list = f.read()
@@ -21,76 +22,86 @@ class gamelogic:
         for item in init_list:
             item["osu"]["x"] = math.ceil(item["osu"]["x"]) % 800
             item["osu"]["y"] = math.ceil(item["osu"]["y"]) % 650
-        # for i in init_list:
-        #     for key, value in i.items():
-        #         print(key, value)
         for i in range(0, len(init_list), (5 - difficulty)):
             self.note_list.append(init_list[i])
 
+    # def get_beatmap(self, file = "output.txt"):
+    #     """
+    #     @pre none
+    #     @param file: the beatmap file
+    #     @post gets beatmap out of file. Each note gets two indexes, 1) Note number and activation frame and 2) Directions
+    #     @return beatmap: a list of all notes in a beatmap
+    #     """
+    #     f = open(file, 'r')
+    #     if f.mode == 'r':
+    #         beatmap = f.read().split('\n')
+    #     return beatmap
 
-    def get_beatmap(self, file = "output.txt"):
-        """
-        @pre none
-        @param file: the beatmap file
-        @post gets beatmap out of file. Each note gets two indexes, 1) Note number and activation frame and 2) Directions
-        @return beatmap: a list of all notes in a beatmap
-        """
-        f = open(file, 'r')
-        if f.mode == 'r':
-            beatmap = f.read().split('\n')
-        return beatmap
+    # def convert_beatmap(self, beatmap):
+    #     """
+    #     @pre none
+    #     @param beatmap: a list of notes where each note has two indexes, 1) Nonte number & activation frame, 2) Directions
+    #     @post converts the beatmap to a dictionary of notes which contains their Directions
+    #     @return beatmap_arr: the converted beatmap
+    #     """
+    #     beatmap_arr = []
+    #     for i in range(1, len(beatmap), 2):
+    #         current_note = ast.literal_eval(beatmap[i])
+    #         beatmap_arr.append(current_note)
+    #     return beatmap_arr
 
-    def convert_beatmap(self, beatmap):
-        """
-        @pre none
-        @param beatmap: a list of notes where each note has two indexes, 1) Nonte number & activation frame, 2) Directions
-        @post converts the beatmap to a dictionary of notes which contains their Directions
-        @return beatmap_arr: the converted beatmap
-        """
-        beatmap_arr = []
-        for i in range(1, len(beatmap), 2):
-            current_note = ast.literal_eval(beatmap[i])
-            beatmap_arr.append(current_note)
-        return beatmap_arr
+    # def get_activation_frames(self, beatmap):
+    #     """
+    #     @pre none
+    #     @param beatmap: a list of notes where each note has two indexes, 1) Nonte number & activation frame, 2) Directions
+    #     @post gets a list of activation frames
+    #     @return af_arr: list of activation frames
+    #     """
+    #     af_arr = []
+    #     for i in range(0, len(beatmap)-1, 2):
+    #         frame = int(beatmap[i].split(": ")[1])
+    #         af_arr.append(frame)
+    #     return af_arr
 
-    def get_activation_frames(self, beatmap):
-        """
-        @pre none
-        @param beatmap: a list of notes where each note has two indexes, 1) Nonte number & activation frame, 2) Directions
-        @post gets a list of activation frames
-        @return af_arr: list of activation frames
-        """
-        af_arr = []
-        for i in range(0, len(beatmap)-1, 2):
-            frame = int(beatmap[i].split(": ")[1])
-            af_arr.append(frame)
-        return af_arr
+    # def generate_notelist(self, beatmap_arr, frames, width, height):
+    #     """
+    #     @pre None
+    #     @param beatmap_arr: list of note dictionaries (w/ directions)
+    #     @param frames: list of activation frames
+    #     @param width: note width
+    #     @param height: note height
+    #     @post generates a list of pygame rects corresponding to each note
+    #     @return note_list: list of rects
+    #     """
+    #     note_list = []
+    #     act_time = frames[0]
+    #     for i in range(len(beatmap_arr)):
+    #         act_time = frames[i]/44100              #second at which it should be activiated
+    #         act_time = act_time*300
+    #         act_time = act_time + 550
+    #         if beatmap_arr[i]['Up']:
+    #             note_list.append(pygame.Rect((200, -act_time), (width, height)))
+    #         elif beatmap_arr[i]['Down']:
+    #             note_list.append(pygame.Rect((400, -act_time), (width, height)))
+    #         elif beatmap_arr[i]['Left']:
+    #             note_list.append(pygame.Rect((0, -act_time), (width, height)))
+    #         else:
+    #             note_list.append(pygame.Rect((600, -act_time), (width, height)))
+    #     return note_list
 
-    def generate_notelist(self, beatmap_arr, frames, width, height):
-        """
-        @pre None
-        @param beatmap_arr: list of note dictionaries (w/ directions)
-        @param frames: list of activation frames
-        @param width: note width
-        @param height: note height
-        @post generates a list of pygame rects corresponding to each note
-        @return note_list: list of rects
-        """
-        note_list = []
-        act_time = frames[0]
-        for i in range(len(beatmap_arr)):
-            act_time = frames[i]/44100              #second at which it should be activiated
-            act_time = act_time*300
-            act_time = act_time + 550
-            if beatmap_arr[i]['Up']:
-                note_list.append(pygame.Rect((200, -act_time), (width, height)))
-            elif beatmap_arr[i]['Down']:
-                note_list.append(pygame.Rect((400, -act_time), (width, height)))
-            elif beatmap_arr[i]['Left']:
-                note_list.append(pygame.Rect((0, -act_time), (width, height)))
+    def map(self, width, height):
+        self.map = []
+        at = 0
+        for item in self.note_list:
+            at = ((item["act_frame"]/44100) * 300) + 550
+            if item["rb"]["button"] == 'U':
+                self.map.append(pygame.Rect((200, -at), (width, height)))
+            elif item["rb"]["button"] == 'D':
+                self.map.append(pygame.Rect((400, -at), (width, height)))
+            elif item["rb"]["button"] == 'L':
+                self.map.append(pygame.Rect((0, -at), (width, height)))
             else:
-                note_list.append(pygame.Rect((600, -act_time), (width, height)))
-        return note_list
+                self.map.append(pygame.Rect((600, -at), (width, height)))
 
     def run_standard(self, song="song.wav"):
         """
@@ -123,19 +134,22 @@ class gamelogic:
         screen = pygame.display.set_mode(SIZE)
         pygame.display.set_caption("Keyboard Hero")
 
-        beatmap = self.get_beatmap(beatmap_file)
-        frames = self.get_activation_frames(beatmap)
-        beatmap_seq = self.convert_beatmap(beatmap)
-        note_list = self.generate_notelist(beatmap_seq, frames, note_width, note_height)
+        # beatmap = self.get_beatmap(beatmap_file)
+        # frames = self.get_activation_frames(beatmap)
+        # beatmap_seq = self.convert_beatmap(beatmap)
+        # note_list = self.generate_notelist(beatmap_seq, frames, note_width, note_height)
+
+        self.generate()
+        self.map(note_width, note_height)
 
         clock = pygame.time.Clock()
         pygame.mixer.music.load(song)
         pygame.mixer.music.play(0)
 
-        left_button = Buttons.key_button(BUTTON_PINK,0,600,note_width,note_height,'Left',True)
-        up_button = Buttons.key_button(BUTTON_BLUE,200,600,note_width,note_height,'Up',True)
-        down_button = Buttons.key_button(BUTTON_YELLOW,400,600,note_width,note_height,'Down',True)
-        right_button = Buttons.key_button(BUTTON_ORANGE,600,600,note_width,note_height,'Right',True)
+        left_button = key_button(BUTTON_PINK,0,600,note_width,note_height,'Left',True)
+        up_button = key_button(BUTTON_BLUE,200,600,note_width,note_height,'Up',True)
+        down_button = key_button(BUTTON_YELLOW,400,600,note_width,note_height,'Down',True)
+        right_button = key_button(BUTTON_ORANGE,600,600,note_width,note_height,'Right',True)
         button_list = [left_button,up_button,down_button,right_button]
 
         score = 0
@@ -155,63 +169,63 @@ class gamelogic:
             if keys[pygame.K_LEFT]:
                 left_button.mouse_over = True
                 left_button.on_click()
-                if left_button.rect.collidelist(note_list) != -1:
-                    index = left_button.rect.collidelist(note_list)
-                    score_mod, current_combo = Scoring.increment_score(left_button.check_hitbox(note_list[index]),current_combo)
+                if left_button.rect.collidelist(self.map) != -1:
+                    index = left_button.rect.collidelist(self.map)
+                    score_mod, current_combo = Scoring.increment_score(left_button.check_hitbox(self.map[index]),current_combo)
                     score += score_mod
-                    del note_list[index] #remove note from list
+                    del self.map[index] #remove note from list
             else:
                 left_button.mouse_over = False
                 left_button.clicked = False
             if keys[pygame.K_UP]:
                 up_button.mouse_over = True
                 up_button.on_click()
-                if up_button.rect.collidelist(note_list) != -1:
-                    index = up_button.rect.collidelist(note_list)
-                    score_mod, current_combo = Scoring.increment_score(up_button.check_hitbox(note_list[index]),current_combo)
+                if up_button.rect.collidelist(self.map) != -1:
+                    index = up_button.rect.collidelist(self.map)
+                    score_mod, current_combo = Scoring.increment_score(up_button.check_hitbox(self.map[index]),current_combo)
                     score += score_mod
-                    del note_list[index]
+                    del self.map[index]
             else:
                 up_button.mouse_over = False
                 up_button.clicked = False
             if keys[pygame.K_DOWN]:
                 down_button.mouse_over = True
                 down_button.on_click()
-                if down_button.rect.collidelist(note_list) != -1:
-                    index = down_button.rect.collidelist(note_list)
-                    score_mod, current_combo = Scoring.increment_score(down_button.check_hitbox(note_list[index]),current_combo)
+                if down_button.rect.collidelist(self.map) != -1:
+                    index = down_button.rect.collidelist(self.map)
+                    score_mod, current_combo = Scoring.increment_score(down_button.check_hitbox(self.map[index]),current_combo)
                     score += score_mod
-                    del note_list[index]
+                    del self.map[index]
             else:
                 down_button.mouse_over = False
                 down_button.clicked = False
             if keys[pygame.K_RIGHT]:
                 right_button.mouse_over = True
                 right_button.on_click()
-                if right_button.rect.collidelist(note_list) != -1:
-                    index = right_button.rect.collidelist(note_list)
-                    score_mod, current_combo = Scoring.increment_score(right_button.check_hitbox(note_list[index]),current_combo)
+                if right_button.rect.collidelist(self.map) != -1:
+                    index = right_button.rect.collidelist(self.map)
+                    score_mod, current_combo = Scoring.increment_score(right_button.check_hitbox(self.map[index]),current_combo)
                     score += score_mod
-                    del note_list[index]
+                    del self.map[index]
             else:
                 right_button.mouse_over = False
                 right_button.clicked = False
             for button in button_list:
                 button.draw(screen)
 
-            for i in range(len(note_list)):
-                if note_list[i][1] >= 600:
-                    pygame.draw.rect(screen, WHITE, note_list[i])
+            for i in range(len(self.map)):
+                if self.map[i][1] >= 600:
+                    pygame.draw.rect(screen, WHITE, self.map[i])
                 else:
-                    if note_list[i][0] == 0:
-                        pygame.draw.rect(screen, PINK, note_list[i])
-                    elif note_list[i][0] == 200:
-                        pygame.draw.rect(screen, BLUE, note_list[i])
-                    elif note_list[i][0] == 400:
-                        pygame.draw.rect(screen, YELLOW, note_list[i])
+                    if self.map[i][0] == 0:
+                        pygame.draw.rect(screen, PINK, self.map[i])
+                    elif self.map[i][0] == 200:
+                        pygame.draw.rect(screen, BLUE, self.map[i])
+                    elif self.map[i][0] == 400:
+                        pygame.draw.rect(screen, YELLOW, self.map[i])
                     else:
-                        pygame.draw.rect(screen, ORANGE, note_list[i])
-                note_list[i][1] += 5 #Increments on y
+                        pygame.draw.rect(screen, ORANGE, self.map[i])
+                self.map[i][1] += 5 #Increments on y
 
             score_text = hollow.textOutline(font,"Score: " + str(score),GREY,WHITE)
             combo_text = hollow.textOutline(font,"Combo: x" + str(current_combo),GREY,WHITE)
@@ -299,4 +313,4 @@ class gamelogic:
             self.run_osu()
 
 game = gamelogic()
-game.run_game("song.wav", "cry.json", 2, 3)
+game.run_game("song.wav", "cry.json", 1, 3)
