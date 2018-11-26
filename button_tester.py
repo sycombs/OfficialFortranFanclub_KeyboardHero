6a45c81709e888a6abfebbabf5b4b2387da25b42
Daniel Gonzalez <dgonzalez93@gmail.com>
2018-11-25 23:53:23 -0600

    button_tester outputs to testResults.txt

--- button_tester.py
+++ button_tester.py
@@ -7,76 +7,85 @@ from Scoring import *
 
 
 def test_scoring_return_score():
-    print("Increment_score returns correct values for no combo:", end = " ")
+    temp_string = "Increment_score returns correct values for no combo: "
     if (increment_score(1,1) == (1,1)):
-        print("True")
+        temp_string += "True "
     else:
-        print("False")
+        temp_string += "True "
+    return temp_string
 
 def test_scoring_raises_combo_val():
-    print("Increment_score raises exception when trying to pass invalid combo:", end = " ")
+    temp_string = "Increment_score raises exception when trying to pass invalid combo: "
     try:
         temp_score, temp_combo = increment_score(1,90)
-        print("False")
+        temp_string += "False "
     except ValueError:
-        print("True")
+        temp_string += "True "
+    return temp_string
 
 def test_scoring_inc_combo():
-    print("Increment_score correctly increments combo when collisions > 1:", end=" ")
+    temp_string = "Increment_score correctly increments combo when collisions > 1: "
     if (increment_score(2,1) == (2,2)):
-        print("True")
+        temp_string += "True "
     else:
-        print("False")
+        temp_string += "False "
+    return temp_string
 
 def test_scoring_dec_combo():
-    print("Increment_score correctly returns combo to 1 when collisions == 1:", end=" ")
+    temp_string = "Increment_score correctly returns combo to 1 when collisions == 1: "
     if (increment_score(1,2) == (1,1)):
-        print("True")
+        temp_string += "True "
     else:
-        print("False")
+        temp_string += "False "
+    return temp_string
 
 def test_button_get_rect():
-    print("get_rect correctly returns pygame rect object:", end=" ")
+    temp_string = "get_rect correctly returns pygame rect object: "
     button = gui_button((0,0,0), 0, 0, 40, 40, "None")
     if (isinstance(button.get_rect(),pygame.Rect)):
-        print("True")
+        temp_string += "True "
     else:
-        print("False")
+        temp_string += "False "
+    return temp_string
 
 def test_button_on_click():
-    print("test_button correctly activates on_click function:", end=" ")
+    temp_string = "test_button correctly activates on_click function: "
     button = gui_button((0,0,0), 0, 0, 40, 40, "None", None, lambda: True)
     if button.on_click():
-        print("True")
+        temp_string += "True "
     else:
-        print("False")
+        temp_string += "False "
+    return temp_string
 
 def test_key_button_hitbox():
-    print("check_hitbox correctly returns number of collisions:", end=" ")
+    temp_string = "check_hitbox correctly returns number of collisions: "
     button = key_button((0,0,0),0,0,40,40,"None")
     rect_no_collisions = pygame.rect.Rect(41,41,10,10)
     rect_three_collisions = pygame.rect.Rect(0,0,40,40)
     if button.check_hitbox(rect_no_collisions) == 0:
         if button.check_hitbox(rect_three_collisions) == 3:
-            print("True")
+            temp_string += "True "
         else:
-            print("False")
+            temp_string += "False "
     else:
-        print("False")
+        temp_string += "False "
+    return temp_string
 
 
 
 def run_tests():
-    print("\nCommencing Button and Scoring test suite:\n ")
-    test_scoring_return_score()
-    test_scoring_raises_combo_val()
-    test_scoring_inc_combo()
-    test_scoring_dec_combo()
-    test_button_get_rect()
-    test_button_on_click()
-    test_key_button_hitbox()
-    print("\nButton and Scoring test suite complete.\n")
-
+    temp_string = """"""
+    temp_string += test_scoring_return_score()
+    temp_string += test_scoring_raises_combo_val()
+    temp_string += test_scoring_inc_combo()
+    temp_string += test_scoring_dec_combo()
+    temp_string += test_button_get_rect()
+    temp_string += test_button_on_click()
+    temp_string += test_key_button_hitbox()
+    with open('testResults.txt','a') as f:
+        f.write(temp_string)
 
 if __name__ == '__main__':
+    print("\nCommencing Button and Scoring test suite:\n ")
     run_tests()
+    print("\nButton and Scoring test suite complete.\n")
