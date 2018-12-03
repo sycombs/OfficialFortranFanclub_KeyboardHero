db3d43b20cab8e93715fa4e5e2c06f7256fef127
EDHidalgo <Hidalgo.Edmundo@gmail.com>
2018-12-03 10:12:58 -0600

    Updated Tester

--- Test.hs
+++ Test.hs
@@ -9,14 +9,13 @@ import qualified BM_Gen as BMG
 main :: IO ()
 main = hspec $ do
   -- I have no idea why these have to be here, but they do
-  let zeroStream = (take 1000000 $ repeat 0    ) :: [Int32]
-  let dupeStream = (take 88200      $ repeat 81273) :: [Int32]
-  let bigD       = 999999999999999999               :: Double
-  let medD       = 99999999                         :: Double
-  let smlD       = 99999                            :: Double
-  let negD       = -44100                           :: Double
+  let zIntStream = take 1000000 $ repeat 0
+  let dupeStream = take 88200 $ repeat 81273
+  let bigD       = 999999999999999999
+  let smlD       = 99999
+  let negD       = -44100
 
-  describe "boundsCheck" $ do
+  describe "Bounds Checker : boundsCheck" $ do
     it "Forces circle within a dimension minimum" $
       BMG.boundsCheck(-1, 50, 850) `shouldBe` 50
 
@@ -26,53 +25,29 @@ main = hspec $ do
     it "Does not change anything when circle within bounds" $
       BMG.boundsCheck(0, 0, 0)  `shouldBe` 0
 
-  -- describe "genButton" $ do
-  --   it "Doesn't crash with negative values" $
-  --     BMG.genButton(-1) `shouldBe` 'R'
+  describe "Generate Button : genButton" $ do
+    it "Returns a valid out when fed a negative value" $
+      BMG.genButton(-1) `shouldBe` Just 'U'
 
-  describe "differenceFinder" $ do
-    it "Returns [] when fed a huge stream of zeros" $
-      length (BMG.differenceFinder bigD zeroStream) `shouldBe` 0
+    it "Returns Nothing when fed a multiple of 5" $
+      BMG.genButton(5 * 5) `shouldBe` Nothing
 
-    it "Returns [] when fed a medium size stream of zeros" $
-      length (BMG.differenceFinder medD zeroStream) `shouldBe` 0
+  describe "Percent Difference : pctDiff" $ do
+    it "Returns Nothing when fed inputs of 0" $
+     (BMG.pctDiff 0 0) `shouldBe` Nothing
 
-    it "Returns [] when fed a huge small of zeros" $
-      length (BMG.differenceFinder smlD zeroStream) `shouldBe` 0
+    it "Returns Nothing when fed identical inputs" $
+     (BMG.pctDiff (-15) (-15)) `shouldBe` Nothing
 
-    it "Returns [] when fed a negative frame counter" $
-      length (BMG.differenceFinder negD zeroStream) `shouldBe` 0
-
-    it "Returns [] when fed a stream of duplicates" $
-      length (BMG.differenceFinder medD dupeStream) `shouldBe` 0
+    it "Returns Nothing when fed additive inverse inputs" $
+     (BMG.pctDiff 1000 (-1000)) `shouldBe` Nothing
 
   describe "findImpulse" $ do
-    it "Returns [] when fed a huge stream of zeros" $
-      length (BMG.findImpulse bigD zeroStream) `shouldBe` 0
+    it "Returns a list of length 0 when fed a huge stream of zeros" $
+      length(BMG.findImpulse bigD zIntStream)  `shouldBe` 0
 
-    it "Returns [] when fed a medium size stream of zeros" $
-      length (BMG.findImpulse medD zeroStream) `shouldBe` 0
+    it "Returns a list of length 0 when fed a stream of duplicates" $
+      length(BMG.findImpulse smlD dupeStream)  `shouldBe` 0
 
-    it "Returns [] when fed a huge small of zeros" $
-      length (BMG.findImpulse smlD zeroStream) `shouldBe` 0
-
-    it "Returns [] when fed a negative frame counter" $
-      length (BMG.findImpulse negD zeroStream) `shouldBe` 0
-
-    it "Returns [] when fed a stream of duplicates" $
-      length (BMG.findImpulse medD dupeStream) `shouldBe` 0
-
-  -- describe "main" $ do
-  --   it "Doesn't blow up when the song isn't there" $
-  --     BMG.main `shouldReturn` ()
-
-{-
---
-  Test ideas :
-    Pass a stream of all 0's
-    Don't pass anything at all
-    Don't allow it to write... do stuff...
--}
-
--- it "Should not accept a fractional number" $
---   BMG.genButton(1.5) `shouldBe` 'A'
+    it "Functions normally even when given a negative starting frame" $
+      length (BMG.findImpulse negD zIntStream) `shouldBe` 0
